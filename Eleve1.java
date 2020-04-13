912f927e4195d95ab96d50a29a9e4f5fa35af242
Laaaura34 <60789487+Laaaura34@users.noreply.github.com>
2020-04-13 15:30:54 +0200

    Update Eleve1.java

--- Eleve1.java
+++ Eleve1.java
@@ -1,52 +1,32 @@
+package projet.serveurclient;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
+
+import projet.serveurclient.WriterThread;
 
 public class Eleve1 {
 	static final int port= 6148;
 	
-	public static void main (String[]args) throws Exception {
-			Socket socket = new Socket ("127.0.0.1", port);
+		public static void main (String[] args) throws IOException {
+			Socket socket= new Socket ("127.0.0.1", port);
 			System.out.println("SOCKET =" + socket);
-		
 			
-			while(true) {
-			BufferedReader plec = new BufferedReader (
-									new InputStreamReader(socket.getInputStream())
-								);
-		
-			PrintWriter pred= new PrintWriter(
-									new BufferedWriter( 
-											new OutputStreamWriter (socket.getOutputStream())),
-								true);
-		
-	
-				Scanner scanner = new Scanner (System.in);
-				for ( int i=0; i<5;i++) {
-					 String scn = scanner.nextLine();
-					pred.println(scn);
-					String str = plec.readLine();
-					if (str.length()<50) {
-						System.out.println("Client reçoit:"+str);
-					} else {
-						System.out.println("Erreur! Trop de caractère");
-						i--;
-					}
-				}
-			
+			BufferedReader bw = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+			PrintWriter out= new PrintWriter(new BufferedWriter( new OutputStreamWriter (socket.getOutputStream())),true);
 
-			System.out.println("FIN CLIENT");
-			pred.close();
-		    plec.close();
-		    socket.close();
-		    scanner.close();
-		}
+	
+				Thread Envoie = new Thread(new WriterThread(out));
+				Envoie.start();
 			
-		
-	  }
+				while(true) {
+					System.out.println(bw.readLine());
+				}
+	
+		}
 }
