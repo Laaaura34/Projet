dccef451f3f0753558fb857c7de3434448a2c49f
Laaaura34 <60789487+Laaaura34@users.noreply.github.com>
2020-04-17 15:35:01 +0200

    Update WriterThread.java

--- WriterThread.java
+++ WriterThread.java
@@ -2,6 +2,7 @@ package projet.serveurclient;
 
 import java.io.PrintWriter;
 import java.util.Scanner;
+import projet.serveurclient.ThreadClient;
 
 public class WriterThread implements Runnable {
 	PrintWriter out;
@@ -12,12 +13,16 @@ public class WriterThread implements Runnable {
 	}
 	public void run() {
 		Scanner scanner = new Scanner (System.in);
+			//Boucle qui dure 5+5 messages
+			//Si à la fin des 5+5 messages le mot n'est pas trouver : Perdu. 
 			while(true && i<5) {
-				mess=scanner.nextLine();
+				mess= scanner.nextLine();
+				// On vérifie si la longueur du message est inférieur à 50 caractères. 
+				// Sinon on propose au joueur de retenter sa chance.
 				if(mess.length()<50) {
 					out.println(mess);
 				} else {
-					System.out.println("Message du Serveur : Trop de caractères! Reécrire le message.");
+					System.out.println("Message du Serveur : Trop de caractères! Réecrire le message.");
 					i--;
 				}
 				i++;
@@ -26,4 +31,5 @@ public class WriterThread implements Runnable {
 			scanner.close();
 		}
 }
+
 
