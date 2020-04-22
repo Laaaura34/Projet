69b7067e698ca05a6d5416b157902e4a8793592d
Laaaura34 <60789487+Laaaura34@users.noreply.github.com>
2020-04-22 23:57:44 +0200

    Update Joueur1.java

--- Joueur1.java
+++ Joueur1.java
@@ -7,18 +7,19 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import projet.serveurclient.ThreadClient1;
 
 import projet.serveurclient.WriterThread;
-
+//Joueur1.java : Classe Joueur1 qui correspond à une classe client. 
+//Va pouvoir communiquer avec le joueur 2 grâce à la classe WriterThread.java.
 public class Joueur1 {
 	static final int port= 8000;
-	static int local;
+	
 					
 		public static void main (String[] args) throws IOException {
 			Socket socket= new Socket ("127.0.0.1", port);
-			System.out.println("SOCKET =" + socket);
+			System.out.println("Bienvenue sur la partie Joueur 1");
 			
-			local=socket.getLocalPort();
 			
 			BufferedReader bw = new BufferedReader (new InputStreamReader(socket.getInputStream()));
 			PrintWriter out= new PrintWriter(new BufferedWriter( new OutputStreamWriter (socket.getOutputStream())),true);
@@ -27,10 +28,12 @@ public class Joueur1 {
 			// Lancement du Thread 'WriterThread'
 			Thread Envoie = new Thread(new WriterThread(out));
 			Envoie.start();
-			
+
 			while(true) {
 				System.out.println(bw.readLine());
 			}
 	
 		}
+		
 }
+
