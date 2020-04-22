393734759f1eacefbba809ca11866f9bc62eaba8
Laaaura34 <60789487+Laaaura34@users.noreply.github.com>
2020-04-22 23:56:06 +0200

    Update Serveur.java

--- Serveur.java
+++ Serveur.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import projet.serveurclient.ThreadClient;
+import projet.serveurclient.ThreadClient1;
+
+//Serveur.java : Classe serveur qui permet le lancement et la synchronisation des ThreadClient 1 et 2.
+//Le serveur attend que les 2 joueurs se connectent afin de pouvoir les accepter et démarrer la communication.
+//Il peut gérer plusieurs communications clientes à la fois grâce au Multithreading.
 
 public class Serveur {
 	String hostName ="127.0.0.1";
@@ -14,27 +18,32 @@ public class Serveur {
 			System.out.println("Serveur écoute");
 			ServerSocket s = null;
 		try {
+			
 			//Création socket serveur
 			s = new ServerSocket(portNumber);
 			while(true) {
 				
-				//Connexion du client1
+				
+				//Connexion du Joueur1
 				Socket soc1;
 				soc1=s.accept();
-			System.out.println("Nouveau Joueur 1 connecté:"+soc1);
+			System.out.println("Nouveau Joueur 1 connecté: "+soc1);
 			
-				//Connexion du client 2
+			
+				//Connexion du Joueur2
 				Socket soc2;
 				soc2=s.accept();
-			System.out.println("Nouveau Joueur 2 connecté:"+soc2);
+			System.out.println("Nouveau Joueur 2 connecté: "+soc2);
 			
-				//Lancement des threads
-				new ThreadClient(soc1,soc2).start();
-				new ThreadClient(soc2,soc1).start();
+			
+				//Lancement des threads grâce à la classe ThreadClient
+				new ThreadClient1(soc1,soc2).start();
+				new ThreadClient2(soc1,soc2).start();
+				
 			}
 		}catch(IOException e){
 			e.printStackTrace();
 		}
 	}
 	
-}	
+}		
