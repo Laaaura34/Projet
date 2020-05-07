6f6a9f147d473b7da24a0c2a63d8e176f38614bd
Laaaura34 <60789487+Laaaura34@users.noreply.github.com>
2020-05-07 14:48:04 +0200

    Update WriterThread.java

--- WriterThread.java
+++ WriterThread.java
@@ -1,42 +1,49 @@
 package projet.serveurclient;
 
-import java.io.PrintWriter;
-import java.util.Scanner;
-import projet.serveurclient.ThreadClient1;
-
 //WriterThread.java : Classe WriterThread qui permet aux Joueurs de pouvoir écrire un message et d'être renvoyé à l'autre joueur. 
 //Comprend les conditions suivantes : 
-// -> Si la longueur du message dépasse les 50 caractères (sans compter les espaces),
+//-> Si la longueur du message dépasse les 50 caractères,
 //le joueur à une seconde chance pour réecrire son message.
-// -> La taille maximale de l'échange entre les deux joueurs ne dépassera pas les 5+5 messages.
+//-> La taille maximale de l'échange entre les deux joueurs ne dépassera pas les 5+5 messages.
 
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class WriterThread implements Runnable {
 	PrintWriter out;
 	String mess= null;
-	int i=0;
+	int i=0; // itérateur initialisé à 0
+	
+	// Constructeur de la classe WriterThread.java qui prend en compte un flux de sortie : out.
 	public WriterThread(PrintWriter out1) {
 		out=out1;	
 	}
+	
 	public void run() {
 		Scanner scanner = new Scanner (System.in);
+		
 			//Boucle qui dure 5+5 messages
-			//Si à la fin des 5+5 messages le mot n'est pas trouver : Perdu. 
+			//Si à la fin des 5+5 messages le mot n'est pas trouvé : Perdu. 
+		
 			while(true && i<5) {
 				mess= scanner.nextLine();
+				
 				// On vérifie si la longueur du message est inférieur à 50 caractères. 
 				// Sinon on propose au joueur de retenter sa chance.
 				if(mess.replaceAll(" ", "").length()<50) {
 					out.println(mess);
+					
 				} else {
 					System.out.println("Message du Serveur : Trop de caractères! Réecrire le message.");
-					i--;
+					i--; 	// Si trop de caractères, l'itérateur prend -1 pour laisser une chance de plus. 
+
 				}
 				i++;
 			}
+			
+			//Si les 5+5 messages passés : Fin de la conversation.
 			System.out.println("Message du Serveur : Fin de la conversation. Perdu!");
 			scanner.close();
 		}
 
 }
-
